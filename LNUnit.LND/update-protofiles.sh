098f1ca973b690308bab41fe73731fe1b9a42497
Richard Safier <richardj@safier.com>
2025-10-13 12:59:18 -0400

    update bitcoind to 30.0. pin loopd protofiles to released version.

--- LNUnit.LND/update-protofiles.sh
+++ LNUnit.LND/update-protofiles.sh
@@ -16,7 +16,7 @@ mkdir -p ./Grpc/looprpc
 mkdir -p ./Grpc/swapserverrpc
 
 LND_TAG=${1:-v0.20.0-beta.rc1}
-LOOP_TAG=${2:-master}
+LOOP_TAG=${2:-v0.31.2-beta}
 
 
 wget -O ./Grpc/lightning.proto https://raw.githubusercontent.com/lightningnetwork/lnd/$LND_TAG/lnrpc/lightning.proto
